aae590fa7b1792c724feea375d888f650bb8e6b7
keithunt-35 <tugume keith022@gmail.com>
2025-06-20 09:01:26 +0300

    trouble shot

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,14 +14,13 @@
                                 class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab
                                 2</a>
                         </li>
-                        @foreach ($categories as $category)
-                            <li class="me-2">
-                                <a href="#"
-                                    class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">
-                                    {{ $category->name }}
-                                </a>
-                            </li>
-                        @endforeach
+                    {{-- 
+                        Iterates over the $categories collection and generates a list item (<li>) for each category.
+                        Each list item contains a link (<a>) to the category's detail page using the 'categories.show' route and the category's ID.
+                        The link displays the category's name and includes styling for hover effects in both light and dark modes.
+                    --}}
+                       
+                       
                     </ul>
 
 
